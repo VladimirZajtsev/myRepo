962e4b1e980dce508ebd1120e53da92653abd263
Android-dev <android-dev@androiddev-pc.(none)>
2012-10-19 17:16:18 +0300

    First commit

---
+++ Menu On Fragments/src/com/example/MenuOnFragments/MyActivity.java
@@ -0,0 +1,35 @@
+package com.example.MenuOnFragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+    }
+
+
+
+    public static class ExampleFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.example_fragment, container, false);
+
+
+        }
+    }
+}
